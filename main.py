b090ac5c483b99b3803de7bbdc6359f3075a345e
Matheus <matheusluis.dev@gmail.com>
2018-10-29 22:15:04 -0300

    Atualização

--- main.py
+++ main.py
@@ -2,50 +2,51 @@ import random
 
 class Deck(object):
     def __init__(self):
-        self.deck_of_cards = []
-        self.base = []
-        for i in range(0,13):
-            troca = {0: 'A', 10: 'J', 11: 'Q', 12: 'K'}
-            if not i in (0, 10, 11, 12):
-                self.base.append(i+1)
-            else:
-                self.base.append(troca.get(i))
-            
+        self.deck_of_cards = list()
+        letter = {0: 'A', 10: 'J', 11: 'Q', 12: 'K'}
+        suit =  {0: '.H', 1: '.D', 2: '.C', 3: '.S'}
+
         for i in range(0, 4):
-            self.deck_of_cards.append(self.base.copy())
+            for j in range(0,13):
+                if not j in (0, 10, 11, 12):
+                    self.deck_of_cards.append(str(j+1)+suit.get(i))
+                else:
+                    self.deck_of_cards.append(letter.get(j)+suit.get(i))
 
 
     def print_deck(self):
-        for i in range(0, len(self.deck_of_cards)):
-            suit = self.deck_of_cards[i]
-            switch = {0: 'Espadas: ', 1: 'Paus: ', 2: 'Copas: ', 3: 'Ouros: '}
-            string = switch.get(i)
-            print(string, suit)
+        print(self.deck_of_cards)
 
 
-    def random_card(self, quantity):
+    def get_card(self, quantity):
         self.quantity = quantity
+        cards = list()
         for i in range(0, self.quantity):
-            size_suit = len(self.deck_of_cards) > 0
-            if size_suit:
-                d = 1
-            else:
-                exit('Baralho simplesmente acabou, como tu fez isso em um blackjack cidadão???') #Isso nunca vai acontecer, mas tamo aí
-            suit = random.randint(0, len(self.deck_of_cards) - d)
+            var = self.deck_of_cards.pop(-1)
+            cards.append(var)
+            #self.deck_of_cards.insert(0, var) --READICIONAR CARTAS AO BARALHO QUANDO EMBARALHAR
+        return cards
 
-            size_card = len(self.deck_of_cards[suit]) > 0
-            if size_card:
-                d = 1
-            else:
-                d = 0
-                self.deck_of_cards[suit] = []
-                print(self.deck_of_cards)
-            card = random.randint(0, len(self.deck_of_cards[suit]) - d)
 
-            self.deck_of_cards[suit].pop(card)
+    def card_shuffle(self):
+        self.deck_of_cards = random.sample(self.deck_of_cards, len(self.deck_of_cards))
+
+    
+    def draw_card(self, card):
+        self.card = card
+        for items in self.card:
+            suit = items[2]
+            card = items[0]
+
+        print('-' * 20)
+        for i in range(0, 12):
+            print('|', end='')
+            print(suit.center(' ', 20), end='')
+            print(card, end='')
+            print('|', end='')
+        print('-' * 20)
             
             
-
 class Player(object):
     def __init__(self, name, age, cash):
         self.name = name
@@ -68,17 +69,16 @@ class Hand(object):
 
 
 for i in range(0,2):
-    print('=' * 150)
+    print('.' * 150)
 
-print(' Bem-vindo ao BlackJack(21) em Python '.center(150, '='))
+print(' Blackjack(21) '.center(150, '+'))
 
 for i in range(0,2):
-    print('=' * 150)
+    print('.' * 150)
 
 dck = Deck()
-
-for i in range(0, 90):
-    dck.random_card(1)
-    print('')
-    dck.print_deck()
-
+dck.card_shuffle()
+card = dck.get_card(1)
+print(card)
+dck.print_deck()
+dck.draw_card(card)
